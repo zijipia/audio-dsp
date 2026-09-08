248a54a9f3865b08f114a5cdabf930cdde989361
Ziji <104454302+zijipia@users.noreply.github.com>
2026-09-08 12:12:11 +0900

    core: add miniaudio DSP data source adapter

---
+++ native/dsp_data_source.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "miniaudio.h"
+#include <cstddef>
+
+namespace audio_dsp {
+
+using ProcessFn = ma_result (*)(void* dsp, const void* input, void* output, size_t frameCount);
+using ResetStateFn = void (*)(void* dsp);
+
+struct DSPDataSource {
+    ma_data_source_base base{};
+    ma_data_source* upstream = nullptr;
+    void* dsp = nullptr;
+    ProcessFn process = nullptr;
+    ResetStateFn resetState = nullptr;
+    ma_format format = ma_format_unknown;
+    ma_uint32 channels = 0;
+    ma_uint32 bytesPerFrame = 0;
+    ma_uint64 cursor = 0;
+};
+
+ma_result dsp_data_source_init(
+    DSPDataSource* self,
+    ma_data_source* upstream,
+    void* dsp,
+    ProcessFn process,
+    ResetStateFn resetState);
+
+void dsp_data_source_uninit(DSPDataSource* self);
+
+ma_data_source* dsp_data_source_get(DSPDataSource* self);
+
+} // namespace audio_dsp
